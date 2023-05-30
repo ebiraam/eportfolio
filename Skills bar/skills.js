9ac877e80dfdf51c76f11fd8449d4aabb39aa41d
ebiraam <43121257+ebiraam@users.noreply.github.com>
2023-05-30 19:36:10 +0300

    v2

---
+++ Skills bar/skills.js
@@ -0,0 +1,7 @@
+ 
+var navbarCollapseBtn = document.querySelector('.navbar-collapse-btn');
+var navbarCollapse = document.querySelector('.navbar-collapse');
+
+navbarCollapseBtn.addEventListener('click', function() {
+  navbarCollapse.classList.toggle('show');
+});
